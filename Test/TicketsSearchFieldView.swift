73b1fb9dbc356ea0543730f758f7ba94fe73442a
YonYoon <zhzhzh1806@gmail.com>
2024-06-08 14:39:01 +0500

    Extract TicketsSearchFieldView to file

---
+++ Test/TicketsSearchFieldView.swift
@@ -0,0 +1,60 @@
+//
+//  TicketsSearchFieldView.swift
+//  Test
+//
+//  Created by Zhansen Zhalel on 08.06.2024.
+//
+
+import SwiftUI
+
+struct TicketsSearchFieldView: View {
+    @SceneStorage("TicketsSearchView.fromWhere") private var fromWhere = ""
+    @State private var isPresented = false
+    
+    var body: some View {
+        Group {
+            HStack(spacing: 16) {
+                Image(systemName: "magnifyingglass")
+                
+                VStack {
+                    TextField("Откуда - Москва", text: $fromWhere)
+                    
+                    Divider()
+                        .overlay {
+                            Color(.testGrey6)
+                        }
+                    
+                    HStack {
+                        Button("Куда - Турция") {
+                            isPresented.toggle()
+                        }
+                        .foregroundStyle(.tertiary)
+                        .sheet(isPresented: $isPresented) {
+                            CitySearchView(fromWhere: $fromWhere)
+                                .presentationBackground(Color(red: 36/255, green: 37/255, blue: 41/255))
+                        }
+    
+                        Spacer()
+                    }
+                }
+            }
+            .padding([.vertical, .trailing], 16)
+            .padding(.leading, 8)
+            .background {
+                RoundedRectangle(cornerRadius: 16)
+                    .foregroundStyle(Color(.testGrey4))
+            }
+            .shadow(radius: 4, y: 4)
+        }
+        .padding(16)
+        .background {
+            RoundedRectangle(cornerRadius: 16)
+                .foregroundStyle(Color(red: 0.18, green: 0.19, blue: 0.21))
+        }
+    }
+}
+
+#Preview {
+    TicketsSearchFieldView()
+        .preferredColorScheme(.dark)
+}
--- Test/TicketsSearchView.swift
+++ Test/TicketsSearchView.swift
@@ -55,53 +55,6 @@ struct TicketsSearchView: View {
     TicketsSearchView(fetcher: OfferCollectionFetcher())
 }
 
-struct TicketsSearchFieldView: View {
-    @SceneStorage("TicketsSearchView.fromWhere") private var fromWhere = ""
-    @State private var isPresented = false
-    
-    var body: some View {
-        Group {
-            HStack(spacing: 16) {
-                Image(systemName: "magnifyingglass")
-                
-                VStack {
-                    TextField("Откуда - Москва", text: $fromWhere)
-                    
-                    Divider()
-                        .overlay {
-                            Color(.testGrey6)
-                        }
-                    
-                    HStack {
-                        Button("Куда - Турция") {
-                            isPresented.toggle()
-                        }
-                        .foregroundStyle(.tertiary)
-                        .sheet(isPresented: $isPresented) {
-                            CitySearchView(fromWhere: $fromWhere)
-                                .presentationBackground(Color(red: 36/255, green: 37/255, blue: 41/255))
-                        }
-    
-                        Spacer()
-                    }
-                }
-            }
-            .padding([.vertical, .trailing], 16)
-            .padding(.leading, 8)
-            .background {
-                RoundedRectangle(cornerRadius: 16)
-                    .foregroundStyle(Color(.testGrey4))
-            }
-            .shadow(radius: 4, y: 4)
-        }
-        .padding(16)
-        .background {
-            RoundedRectangle(cornerRadius: 16)
-                .foregroundStyle(Color(red: 0.18, green: 0.19, blue: 0.21))
-        }
-    }
-}
-
 struct MusicTravelView: View {
     var offer: Offer
     
